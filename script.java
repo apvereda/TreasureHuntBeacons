4e0ddb59ada847e46fbddd4a69ca44ed21e9e66c
Alejandro Perez Vereda <aperezvereda@gmail.com>
2018-03-08 19:24:26 +0100

    Create script.java

---
+++ script.java
@@ -0,0 +1,10 @@
+String URL = "https://goo.gl/Fxezzb";
+if (utils.count("\"Treasure_Find_Game\" : \"treasure\"") == 1) {
+	utils.append("Treasure_Find_Game", "\"treasure\": {
+						\"value\": \"Treasure 02, Search the way\", 
+						\"hint\": \"It seems that the NORTH face is the best way\"}");
+	utils.notify("It seems that the NORTH face is the best way");
+	String name = utils.get("\"Treasure_Find_Game\" : \"player\"");
+	Calendar now = Calendar.getInstance();
+	utils.upload(URL, "utils.notify(\"player "+name+" already got this hint at "+now.get(Calendar.HOUR_OF_DAY)+":"+now.get(Calendar.MINUTE)+"\")");
+}
